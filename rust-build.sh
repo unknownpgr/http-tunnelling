65a185fa66ba60612299a1434c76e05a5085f816
unknownpgr <unknownpgr@gmail.com>
2023-03-11 12:55:50 +0000

    Fix rust build script

--- rust-build.sh
+++ rust-build.sh
@@ -1,11 +1,11 @@
 set -e
 
 cd "$(dirname "$0")"
+
+mkdir -p rust-dist
+rm -rf rust-dist/*
+
 cd rust
-
-mkdir -p ../rust-dist
-rm -rf ../rust-dist/*
-
 cargo build --release
 mv target/release/server ../rust-dist/server
 mv target/release/worker ../rust-dist/worker
